43e31031a4cddd5866ca5d4a8be05a9fa446be17
alco99 <76519958+alco99@users.noreply.github.com>
2021-01-21 17:28:07 +0800

    Create final_main.c

---
+++ final_main.c
@@ -0,0 +1,347 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <ctype.h> 
+#include <string.h> 
+#include <math.h>
+#include <time.h>
+#include <windows.h> 
+#include "function.h"
+
+
+int main() {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7 | FOREGROUND_INTENSITY);
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");	
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■         ■■  ■■     ■■     ■   ■    ■■     ■■■     ■■    ■     ■    ■       ■\n");
+	printf("■        ■  ■■  ■  ■    ■  ■■  ■  ■    ■  ■    ■  ■    ■  ■      ■  ■        ■\n");
+	printf("■        ■  ■■  ■  ■    ■  ■ ■ ■  ■    ■   ■■■   ■    ■  ■       ■■         ■\n");
+	printf("■        ■  ■■  ■  ■    ■  ■  ■■  ■    ■  ■        ■    ■  ■        ■          ■\n");
+	printf("■        ■  ■■  ■    ■■    ■   ■     ■■    ■          ■■    ■■■    ■          ■\n");
+	printf("■           	                                                                                ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");	
+	printf("■                                                                                              ■\n");	
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                            by 鹹魚製作                       ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	puts("");
+	system("pause");
+	system("cls");	//清除畫面
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■            Monopoly                                                                          ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■           	                                                                                ■\n");
+	printf("■            製 作 成 員 :                                                                     ■\n");
+	printf("■                                                                                              ■\n");	
+	printf("■                  §D0915696黃湘紜                                                             ■\n");
+	printf("■                                                                                              ■\n");	
+	printf("■                  §D0915725賴螢葶                                                             ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■          	    §D0948329陳昕伶                                                             ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■          	    §D0948524陳彥勻                                                             ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■            §製作日期 : 2020 / 12 / 5 ~                                                       ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                          →Back to menu please enter 1       ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                          →Want to leave please enter 0      ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	puts("");
+	system("pause");
+	system("cls");	//清除畫面
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■            Monopoly   RULE                                                                   ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■            遊戲開始                                                                          ■\n");
+	printf("■            *遊戲人數4人                                                                      ■\n");
+	printf("■            *遊戲方向為順時針進行                                                             ■\n");
+	printf("■            *各玩家起始財產金額10000元                                                        ■\n");	
+	printf("■                                                                                              ■\n");	
+	printf("■            特殊地點                                                                          ■\n");
+	printf("■            *行走過起點獲得金額1000元                                                         ■\n");
+	printf("■            *行走到監獄限制行動，下一回合骰子骰到偶數時，再下一回合即可恢復行動               ■\n");
+	printf("■            *行走到機會/命運，隨機發生一件事                                                  ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■            交易                                                                              ■\n");
+	printf("■            *行走到一般星球，且該星球無人佔領，則可選擇是否購買                               ■\n");
+	printf("■            *若再次走到自己已佔領的星球，則可選擇是否升級該星球(升級上限為3)                  ■\n");
+	printf("■            *若行走到其他玩家佔領的星球，需繳交過路費(與該星球等級相應金額)給該星球的佔領者   ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■            破產                                                                              ■\n");
+	printf("■            *玩家財產歸零，該玩家退出遊戲                                                     ■\n");
+	printf("■            *若有玩家退出遊戲，則該玩家的星球轉變為空置                                       ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■            勝利條件                                                                          ■\n");
+	printf("■            *回合數太多可選擇是否繼續遊戲，若否，則該玩家退出遊戲                             ■\n");
+	printf("■            *若玩家只剩一人，則倖存者獲勝，遊戲結束                                           ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                          →Back to menu please enter 1       ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                          →Want to leave please enter 0      ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■                                                                                              ■\n");
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	puts("");
+	system("pause");
+	system("cls");	//清除畫面
+	
+	struct player_initialise player[4]={
+		{10000,1,0,0,0,"@                       ", 0} ,
+		{10000,2,0,0,0,"#                       ", 0} ,
+		{10000,3,0,0,0,"&                       ", 0} ,
+		{10000,4,0,0,0,"$                       ", 0}
+	};
+	
+	struct map_initialise map[24]={
+		{"起點",0,0,0,0,0,0},
+		{"水星",0,0,0,0,200,200},
+		{"金星",0,0,0,0,300,300},
+		{"機會",0,0,0,0,0,0},
+		{"火星",0,0,0,0,500,500},
+		{"木星",0,0,0,0,600,600},
+		{"土星",0,0,0,0,700,700},
+		{"小行星帶",0,0,0,0,0,0},
+		{"天王星",0,0,0,0,800,800},
+		{"命運",0,0,0,0,0,0},
+		{"海王星",0,0,0,0,900,900},
+		{"駭客星",0,0,0,0,1000,1000},
+		{"監獄",0,0,0,0,0,0},
+		{"月球",0,0,0,0,1200,1200},
+		{"植物星",0,0,0,0,1400,1400},
+		{"機會",0,0,0,0,0,0},
+		{"方塊星",0,0,0,0,1600,1600},
+		{"美食星",0,0,0,0,1800,1800},
+		{"逢甲星",0,0,0,0,2000,2000},
+		{"太空墳場",0,0,0,0,0,0},
+		{"旺旺星",0,0,0,0,2300,2300},
+		{"命運",0,0,0,0,0,0},
+		{"神秘星",0,0,0,0,2500,2500},
+		{"地球",0,0,0,0,3000,3000},
+	};
+	
+	int i=0;
+	int dice_num,num,m;
+	char respond,ans,symbol,place;
+	map_print(map,player);
+	while(1){
+		//檢查是不是只剩一個玩家
+		int bankruptSituation = 0;
+		bankruptSituation = player[0].bankrupt + player[1].bankrupt + player[2].bankrupt + player[3].bankrupt;
+		if(bankruptSituation==3){   //只剩一個玩家
+			//計算贏家
+			countWinner(map,player);
+			break;
+		}
+		//每個回合會問要不要繼續遊戲
+		if(i==4){
+			printf("是否離開遊戲(Y/y for leave)？");
+			scanf("%c%*c",&respond);
+			//printf("ans=%c  %d\n",respond,respond);
+			printf("\n");
+			if(isalpha(respond) && (respond=='Y' || respond=='y')){   //確定離開遊戲
+				//計算贏家
+				countWinner(map,player);
+				break;
+			}
+			else{  //不繼續，繼續新的回合
+				i=0;
+			}
+		}
+		//跳過破產玩家	
+		if(player[i].bankrupt){
+			i++;
+			continue;
+		}
+		//印出現在玩家
+		printf("\n現在玩家：PLAYER %d\n",i+1);	// +
+		//骰子()
+		dice_num=dice();
+		printf("\n");
+		//移動()
+		if(player[i].move){//如果現在不能動(在監獄)
+			if(dice_num%2){  //骰子點數不是偶數 
+				i++;  //換人 
+				continue;  //離開 
+			}
+			else{
+				player[i].move=0;  //恢復行動並繼續 
+				i++;  //換人 
+				continue;  //離開
+			}
+		}
+		//玩家移動 
+		player[i].location = player[i].location+ dice_num;
+		//超過起點
+		if(player[i].location >= 24){
+			player[i].location = player[i].location-24;
+			printf("\n經過(回到)起點\n\n"); //經過起點獲得1000 
+			player[i].money = player[i].money + 1000;
+		}
+		//印地圖需要更新位置
+		for(m=0; m<24; m++){
+			if(player[i].printOnMap[m] != ' '){
+				symbol = player[i].printOnMap[m];
+				player[i].printOnMap[m] = ' ';
+			}
+		}
+		player[i].printOnMap[player[i].location] = symbol;
+		place = player[i].location;
+		//印地圖
+		map_print(map,player);
+		//判斷
+		switch(player[i].location){
+			case 0://起點
+				
+				break;
+			case 7:
+			case 19://轉角
+				 
+				break;
+			case 3:
+			case 15:
+				//機會
+				chance(player,i);
+				printf("\n");
+				map_print(map,player);
+				printf("\n");
+				break;
+			case 9:
+			case 21: 
+				//命運
+				fate(player,i);
+				printf("\n");
+				map_print(map,player);
+				printf("\n");
+				break;
+			case 12: 
+				//監獄
+				//印地圖
+				printf("\n進到監獄\n");
+				player[i].move = 1; //限制行動 
+				break;
+			default://其他正常地點
+				if(map[place].owner != player[i].realty){  //不是自己的地 
+					if(map[place].owner == 0){  //無主之地
+						printf("\n要不要在這裡買房子？(Y/y for buy) ");
+						scanf("%c%*c", &ans);
+						//printf("ans=%c  %d\n",ans,ans);
+						printf("\n");
+						if(ans=='Y' || ans=='y'){  // && (player[i].money>map[place].price)
+							map[place].own = 1;
+							map[place].level = 1;
+							map[place].owner = player[i].realty;
+							player[i].money = player[i].money-map[place].price;
+							map[place].tolls = map[place].price/2;
+							map_print(map,player);
+							printf("\n");
+						}
+						//else if((ans=='Y' || ans=='y') && player[i].money<map[place].price){
+						//	printf("你沒有那麼多錢！\n\n");
+						//}
+					}
+					else{
+						//扣過路費
+						printf("\n這裡是PLAYER %d的家，請付過路費 %d\n\n", map[place].owner, map[place].tolls);
+						player[i].money = player[i].money-map[place].tolls;
+						num = map[place].owner;
+						player[num-1].money = player[num-1].money+map[place].tolls;
+						map_print(map,player);
+						printf("\n");
+					}
+				}
+				else{   //是自己的地
+					printf("\n歡迎回家\n");
+					if(map[place].level!=3){
+						printf("要不要升級？(Y/y for upgate) ");
+						scanf("%c%*c", &ans);
+						printf("\n");
+						if(ans=='Y' || ans=='y'){  //確定要升級 && player[i].money>(map[place].price*2)
+							player[i].money = player[i].money-map[place].price*2;
+							map[place].level++;
+							map[place].price = map[place].price*2;
+							map[place].tolls = map[place].tolls*2;
+							map_print(map,player);
+							printf("\n");
+						}
+						//else if((ans=='Y' || ans=='y') && player[i].money<(map[place].price*2)){
+						//	printf("你沒有那麼多錢！\n\n");
+						//}
+					}
+		
+			}
+			break;
+		}
+		
+		//檢查破產狀態
+		if(player[i].money<=0){
+			player[i].bankrupt = 1;
+			player[i].money = 0;
+			for(m=0; m<24; m++){
+				if(player[i].printOnMap[m] != ' '){
+					player[i].printOnMap[m] = ' ';
+				}
+			}
+			for(m=0; m<24; m++){
+				if(map[m].owner==player[i].realty){
+					map[m].owner = 0;
+					map[m].own = 0;
+					map[m].level = 0;
+					map[m].price = map[m].original_price;
+					map[m].tolls = 0;
+				}
+			}
+			printf("PLAYER %d 已經破產\n",i+1);
+		}
+		//換人
+		i++;
+//		printf("\n");	// -
+	}
+	return 0;
+
+}
